0a126a017cb38cd5575eda8e2be046cb19a96a87
smmmfrd <smmmfrd@gmail.com>
2023-07-14 10:41:18 -0700

    Undo Icon

--- src/utils/IconsMap.tsx
+++ src/utils/IconsMap.tsx
@@ -98,4 +98,19 @@ export const IconMap = {
       <path d="M11.47 1.72a.75.75 0 011.06 0l3 3a.75.75 0 01-1.06 1.06l-1.72-1.72V7.5h-1.5V4.06L9.53 5.78a.75.75 0 01-1.06-1.06l3-3zM11.25 7.5V15a.75.75 0 001.5 0V7.5h3.75a3 3 0 013 3v9a3 3 0 01-3 3h-9a3 3 0 01-3-3v-9a3 3 0 013-3h3.75z" />
     </svg>
   ),
+  undo: (
+    // ArrowUturnLeft
+    <svg
+      aria-hidden="true"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        clipRule="evenodd"
+        d="M9.53 2.47a.75.75 0 010 1.06L4.81 8.25H15a6.75 6.75 0 010 13.5h-3a.75.75 0 010-1.5h3a5.25 5.25 0 100-10.5H4.81l4.72 4.72a.75.75 0 11-1.06 1.06l-6-6a.75.75 0 010-1.06l6-6a.75.75 0 011.06 0z"
+        fillRule="evenodd"
+      />
+    </svg>
+  ),
 };
