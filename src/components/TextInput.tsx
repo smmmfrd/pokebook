27e2ae7ff53a54b480860d8895cce1fed56f75de
smmmfrd <smmmfrd@gmail.com>
2023-07-14 10:41:29 -0700

    Delete Button Functionality

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { IconMap } from "~/utils/IconsMap";
 
 type TextInputProps = {
   pokemonName: string;
@@ -21,12 +22,17 @@ export default function TextInput({ pokemonName }: TextInputProps) {
     setInputValue((prev) => `${prev} ${pokemonName.slice(0, index + 1)}`);
   }
 
+  function handleDelete() {
+    setInputValue((prev) => prev.substring(0, prev.lastIndexOf(" ")));
+  }
+
   return (
     <form
       className={`mx-auto max-w-sm p-4 ${!open && "cursor-pointer"}`}
       onMouseDown={() => setOpen(true)}
       onSubmit={() => {}}
     >
+      {/* INPUT */}
       <div
         onMouseLeave={handleMouseLeave}
         className={`mb-2 flex scale-y-100 select-none justify-between overflow-hidden border-base-content font-mono text-2xl lowercase transition-all duration-100 ${
@@ -46,6 +52,8 @@ export default function TextInput({ pokemonName }: TextInputProps) {
           </div>
         ))}
       </div>
+
+      {/* TEXT DISPLAY */}
       <div
         className={`textarea-bordered textarea w-full cursor-default overflow-hidden text-xl ${
           !open && "cursor-pointer"
@@ -59,6 +67,18 @@ export default function TextInput({ pokemonName }: TextInputProps) {
           </span>
         )}
       </div>
+
+      {/* DELETE BUTTON */}
+      <button
+        type="button"
+        className={`btn-outline btn ${!open && "hidden"}`}
+        onClick={handleDelete}
+      >
+        <span className="h-4 w-4">{IconMap.undo}</span>
+        DELETE
+      </button>
+
+      {/* CANCEL AND CLOSE BUTTONS */}
       <div className="flex justify-between">
         <button
           type="reset"
