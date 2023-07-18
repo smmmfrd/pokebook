f49b5a98eec6e5ee98a720985d4183dea3ba6287
smmmfrd <smmmfrd@gmail.com>
2023-07-18 15:09:00 -0700

    Refactor Theme Icons

--- src/components/NavbarIcon.tsx
+++ src/components/NavbarIcon.tsx
@@ -2,8 +2,16 @@ import { IconMap } from "~/utils/IconsMap";
 
 type NavbarIconProps = {
   icon: keyof typeof IconMap;
+  styleExtensions?: string;
 };
 
-export default function NavbarIcon({ icon }: NavbarIconProps) {
-  return <div className="h-8 w-8">{IconMap[icon]}</div>;
+export default function NavbarIcon({
+  icon,
+  styleExtensions = "",
+}: NavbarIconProps) {
+  return (
+    <div className={`${styleExtensions} h-8 w-8 fill-current`}>
+      {IconMap[icon]}
+    </div>
+  );
 }
