035799b079c82302f05fcd2bea247067b6efa449
smmmfrd <smmmfrd@gmail.com>
2023-07-18 15:04:15 -0700

    Light and Dark Mode Switching

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 
 import Navbar from "./Navbar";
@@ -12,11 +12,22 @@ export default function Layout({ children }: LayoutProps) {
 
   if (!sessionData) return children;
 
+  const [theme, setTheme] = useState("winter");
+
+  const toggleTheme = () => {
+    setTheme(theme === "winter" ? "night" : "winter");
+  };
+
+  useEffect(() => {
+    document.querySelector("html")?.setAttribute("data-theme", theme);
+  }, [theme]);
+
   return (
     <div className="flex min-w-full">
       <Navbar
         profileImage={sessionData.user.profileImage}
         userId={sessionData.user.id}
+        toggleTheme={toggleTheme}
       />
       <main className="min-h-screen w-full border-l">{children}</main>
     </div>
