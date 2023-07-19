9b7f3ebba27327aef29dfff6cedf192e2a1c51c5
smmmfrd <smmmfrd@gmail.com>
2023-07-19 11:45:52 -0700

    Primary Button State and Styling Fix

--- src/components/ProfileButtons.tsx
+++ src/components/ProfileButtons.tsx
@@ -1,9 +1,53 @@
+import { useSession } from "next-auth/react";
+
+const RELATIONAL_STATES_MAP = {
+  self: {},
+  none: {
+    following: false,
+  },
+  following: {
+    following: true,
+    friends: false,
+  },
+  friends: {
+    following: true,
+    friends: true,
+  },
+};
+
 export default function ProfileButtons({ profileId }: { profileId: string }) {
+  const session = useSession();
+
+  if (session.status !== "authenticated") return <></>;
+
+  // Viewing your profile
+  if (session.data?.user.id === profileId) {
+    return (
+      <div
+        className="tooltip-info tooltip tooltip-left"
+        data-tip="You can't follow or friend yourself!"
+      >
+        <button disabled className="btn-info btn-xs btn">
+          Follow
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <button className="btn-info btn-xs btn">Follow</button>
+        <button className="btn-success btn-xs btn">Friend Req.</button>
+      </>
+    );
+  }
+
+  return <></>;
+
   return (
     <div className="flex gap-2">
-      <button className="btn-info btn-xs btn">Follow</button>
+      {/* <button className="btn-info btn-xs btn">Follow</button> */}
       {/* <button className="btn-error btn-xs btn">Un-Follow</button> */}
-      <button className="btn-success btn-xs btn">Friend Req.</button>
+      {/* <button className="btn-success btn-xs btn">Friend Req.</button> */}
       {/* <button className="btn-error btn-xs btn">Un-Friend</button> */}
     </div>
   );
--- src/pages/profile/[profileId].tsx
+++ src/pages/profile/[profileId].tsx
@@ -66,11 +66,11 @@ export default function ProfilePage({
   return (
     <>
       <header className="sticky top-0 z-20 bg-base-100">
-        <nav className="flex items-center justify-around gap-2 border-b p-4">
+        <nav className="flex items-center justify-between gap-2 border-b p-4">
           <button onClick={() => router.back()} title="Go Back">
             <NavbarIcon icon="arrowLeft" styleExtensions="w-5 h-5" />
           </button>
-          <h1 className="text-2xl font-bold capitalize">{pokemon.name}</h1>
+          <h1 className="grow text-2xl font-bold capitalize">{pokemon.name}</h1>
           <ProfileButtons profileId={profileId} />
         </nav>
         <div className="flex gap-8 p-4">
