dbc1d692b7df483b95857c7d76e54cf0bccd8773
smmmfrd <smmmfrd@gmail.com>
2023-07-27 15:23:39 -0700

    DOM Bug Fx

--- src/utils/IconsMap.tsx
+++ src/utils/IconsMap.tsx
@@ -139,15 +139,15 @@ export const IconMap = {
       aria-hidden="true"
       fill="none"
       stroke="currentColor"
-      stroke-width="1.5"
+      strokeWidth={1.5}
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
         d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      ></path>
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   ),
   heart: (
