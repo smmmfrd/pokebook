19052da1d644f8d97496a3624d8c90cb5c863e4a
smmmfrd <smmmfrd@gmail.com>
2023-08-02 15:10:26 -0700

    Friend Card Styling

--- src/pages/profile/[profileId]/friends.tsx
+++ src/pages/profile/[profileId]/friends.tsx
@@ -1,3 +1,73 @@
-export default function FriendManagement() {
-  return <div>here's ya friends, now lose 'em</div>;
+import { type GetServerSideProps } from "next";
+import { getServerAuthSession } from "~/server/auth";
+import { caller } from "~/server/api/root";
+import { api } from "~/utils/api";
+
+import BackHeader from "~/components/BackHeader";
+import ProfileImage from "~/components/ProfileImage";
+import Head from "next/head";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getServerAuthSession(ctx);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: `/login?returnURL=${encodeURIComponent(ctx.resolvedUrl)}`,
+        permanent: false,
+      },
+    };
+  }
+
+  const pokemonName = `${session.user.pokemonName
+    .slice(0, 1)
+    .toUpperCase()}${session.user.pokemonName.slice(1)}`;
+
+  return {
+    props: { session, user: session.user },
+  };
+};
+
+type FriendManagementProps = {
+  user: {
+    profileImage: string;
+    pokemonName: string;
+    id: string;
+  };
+};
+
+export default function FriendManagement({ user }: FriendManagementProps) {
+  const pokemonName = `${user.pokemonName
+    .slice(0, 1)
+    .toUpperCase()}${user.pokemonName.slice(1)}`;
+  const useGetAllFriends = api.profile.getAllFriends.useQuery();
+
+  return (
+    <>
+      <Head>
+        <title>{`${pokemonName}'s Friends`}</title>
+      </Head>
+      <BackHeader
+        title={`${pokemonName}'s Friends`}
+        headExtensions={<ProfileImage size="small" src={user.profileImage} />}
+      />
+      {useGetAllFriends.isLoading && <div>loading...</div>}
+      {useGetAllFriends.data != null &&
+        useGetAllFriends.data.friends.map((friend) => (
+          <section className="flex items-center gap-4 border-b p-4">
+            <ProfileImage
+              size="large"
+              src={friend.profileImage}
+              styleExtensions="shrink-0"
+            />
+            <div className="flex w-full flex-col gap-2">
+              <h2 className="text-3xl font-bold">{`${friend.pokemon?.name
+                .slice(0, 1)
+                .toUpperCase()}${friend.pokemon?.name.slice(1)}`}</h2>
+              <button className="btn-error btn ml-auto block">Un-friend</button>
+            </div>
+          </section>
+        ))}
+    </>
+  );
 }
