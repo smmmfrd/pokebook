7ebdee9521d36968938f1eaf7ddf42e38bc75606
smmmfrd <smmmfrd@gmail.com>
2023-08-09 15:34:45 -0700

    More Info and Link to Learn More

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,8 @@ import { type GetServerSideProps } from "next";
 import { getServerAuthSession } from "~/server/auth";
 
 import { signIn } from "next-auth/react";
+import Head from "next/head";
+import Link from "next/link";
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const session = await getServerAuthSession(ctx);
@@ -28,22 +30,36 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
 export default function LoginPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center px-12 py-40 sm:flex-row">
-      <header className="max-w-xs basis-1/2 [&>*]:mb-4">
-        <h1 className="text-4xl">Pokébook</h1>
-        <p>
-          To view any content you must sign in as a registered user, or as a
-          guest.
-        </p>
-        <p>User registration is handled by Next Auth.</p>
-      </header>
-      <div className="divider divider-vertical sm:divider-horizontal"></div>
-      <div className="flex basis-1/2 justify-between gap-12 sm:flex-col sm:items-start">
-        <button className="btn-primary btn" onClick={() => void signIn()}>
-          Log In
-        </button>
-        <button className="btn-secondary btn">Guest Log In</button>
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>Sign In | Pokebook</title>
+      </Head>
+      {/* We can return a main here since layout does not render on this page. */}
+      <main className="flex min-h-screen flex-col items-center px-12 py-40 sm:flex-row">
+        <header className="max-w-xs basis-1/2 [&>*]:mb-4">
+          <h1 className="text-4xl">Pokébook</h1>
+          <p>To view any content you must sign in, or as a guest.</p>
+          <p>You must sign in by connecting a Discord account to this app.</p>
+          <p>
+            This connection is handled by the open source authentication service{" "}
+            <Link
+              href={"https://next-auth.js.org/getting-started/introduction"}
+              rel="noopener noreferrer"
+              target="_blank"
+              className="link font-bold"
+            >
+              Next Auth.
+            </Link>{" "}
+          </p>
+        </header>
+        <div className="divider divider-vertical max-h-96 sm:divider-horizontal"></div>
+        <div className="flex basis-1/2 justify-between gap-12 sm:flex-col sm:items-start">
+          <button className="btn-primary btn" onClick={() => void signIn()}>
+            Log In
+          </button>
+          <button className="btn-secondary btn">Guest Log In</button>
+        </div>
+      </main>
+    </>
   );
 }
