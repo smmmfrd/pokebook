3621e24528131b2ff1bb776aa8acac3bde568eab
smmmfrd <smmmfrd@gmail.com>
2023-09-05 12:50:08 -0700

    Add Middleware and Remove Query Logs

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  // This file is in the proper place, we just need to update the matcher.
+  console.log("heyo", request.url);
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next|[\\w-]+\\.\\w+).*)",
+  ],
+};
