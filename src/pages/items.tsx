1d9f74bba930e1ff81360dde58b50977322213c2
smmmfrd <smmmfrd@gmail.com>
2023-09-15 13:57:07 -0700

    Item Type and Count Rendering

--- src/pages/items.tsx
+++ src/pages/items.tsx
@@ -11,7 +11,28 @@ type ItemsPageProps = {
   items: Item[];
 };
 
+type ItemType = {
+  name: string;
+  count: number;
+};
+
 export default function ItemsPage({ items }: ItemsPageProps) {
+  const itemTypes = items.reduce((acc, item) => {
+    const typeIndex = acc.findIndex((type) => type.name === item.itemType);
+    if (typeIndex >= 0) {
+      return acc.map((type, index) =>
+        index === typeIndex
+          ? {
+              ...type,
+              count: type.count + 1,
+            }
+          : type
+      );
+    } else {
+      return [...acc, { name: item.itemType, count: 1 }];
+    }
+  }, [] as ItemType[]);
+
   return (
     <>
       <Head>
@@ -19,12 +40,22 @@ export default function ItemsPage({ items }: ItemsPageProps) {
       </Head>
       <nav className="border-b px-4 py-2">
         <h1 className="text-3xl">Items</h1>
+
+        <ul>
+          {itemTypes.map((type) => (
+            <li>
+              <button>
+                {type.name} - ({type.count})
+              </button>
+            </li>
+          ))}
+        </ul>
       </nav>
-      {items.map((item) => (
+      {/* {items.map((item) => (
         <section>
           <header>{item.name}</header>
         </section>
-      ))}
+      ))} */}
     </>
   );
 }
