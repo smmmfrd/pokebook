3a467216226fd0ed7a45e8f9232ca5ca56e2b767
smmmfrd <smmmfrd@gmail.com>
2023-09-26 11:10:38 -0700

    Custom Hook for Pokemon Data

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,11 +1,13 @@
+import { getCookie } from "cookies-next";
+import { type Session } from "next-auth";
 import { useEffect, useState } from "react";
 
-// Custom hook that manages a local storage item that determines if the user has gone above their limit for the hour of a certain action.
 type StorageData = {
   time: string;
   ticks: number;
 };
 
+// Custom hook that manages a local storage item that determines if the user has gone above their limit for the hour of a certain action.
 const useLimit = (
   userName: string,
   valueName: string,
@@ -65,4 +67,40 @@ const useLimit = (
   return [valid, ticked];
 };
 
-export { useLimit };
+const useUserPokemon = (sessionData: Session | null): UserPokemon => {
+  const [guestPokemon, setGuestPokemon] = useState<UserPokemon | null>(null);
+
+  useEffect(() => {
+    async function getGuest() {
+      const guestCookie = getCookie("guest-pokemon");
+      if (guestCookie != null) {
+        const guestData = await JSON.parse(guestCookie);
+        setGuestPokemon(guestData);
+      }
+    }
+
+    getGuest();
+  }, []);
+
+  if (sessionData != null) {
+    return {
+      id: sessionData.user.pokemonId,
+      name: sessionData.user.pokemonName,
+      profileImage: sessionData.user.profileImage,
+    };
+  }
+
+  if (guestPokemon != null) {
+    return guestPokemon;
+  }
+
+  return { id: 0, name: "", profileImage: "" };
+};
+
+type UserPokemon = {
+  id: number;
+  name: string;
+  profileImage: string;
+};
+
+export { useLimit, useUserPokemon };
