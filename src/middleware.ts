1712fee7189a5cf0d0b22d06e35dc4ed4fdb9437
smmmfrd <smmmfrd@gmail.com>
2023-09-28 15:43:11 -0700

    Fix Production Cookie Name Bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(request: NextRequest) {
   if (
-    request.cookies.get("guest-pokemon") == null &&
-    request.cookies.get("next-auth.session-token") == null
+    // request.cookies.get("guest-pokemon") == null &&
+    request.cookies.get(
+      process.env.NODE_ENV === "production"
+        ? "__Secure-next-auth.session-token"
+        : "next-auth.session-token"
+    ) == null
   ) {
     return NextResponse.redirect(
       new URL(`/login?returnURL=${request.nextUrl.pathname}`, request.url)
